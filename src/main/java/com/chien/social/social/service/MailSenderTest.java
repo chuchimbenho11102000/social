2d12b4761194548c0add24cd3ac4b20e97f96904
chuchimbenho11102000 <91276736+chuchimbenho11102000@users.noreply.github.com>
2022-10-23 13:09:04 +0700

    sendMail service

---
+++ src/main/java/com/chien/social/social/service/MailSenderTest.java
@@ -0,0 +1,26 @@
+package com.chien.social.social.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailSenderTest {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendMail(String toEmail,
+                         String subject ,
+                         String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("nhomtroatc@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        mailSender.send(message);
+
+    }
+}
